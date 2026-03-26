7819d7f2e8c60cbb763d99052b2dcf55b622c00f
PhilippBaumi <philipp.baumgartner@live.at>
2026-03-26 09:41:54 +0100

    Projektdateien hinzufügen.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauiTest
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly ViewModel viewModel = new();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext= viewModel;
+        }
+
+        private async void AddToList(object sender, EventArgs e)
+        {
+            if (viewModel.SelectedDate == null)
+            {
+                await ShowDialog("Kein Datum", "Kein Datum gewählt!");
+                return;
+            }
+            string date = viewModel.SelectedDate.Value.ToString("dd.MM.yyyy");
+            if (viewModel.Dates.Contains(date))
+            {
+                await ShowDialog("Datum vorhanden", "Datum bereits in der Liste");
+                return;
+             }
+            viewModel.Dates.Add(date);
+        }
+
+        private async Task ShowDialog(string title, string text)
+        {
+            bool answer=await DisplayAlertAsync(title, text, "OK", "Abbrechen");
+        }
+
+        private async void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (viewModel.SelectedDate != null)
+            {
+                string? date = viewModel.SelectedText;
+                if (date != null)
+                {
+                    await ShowDialog("Gewähltes Datum", date);
+                }
+                return;
+            }
+        }
+    }
+}
